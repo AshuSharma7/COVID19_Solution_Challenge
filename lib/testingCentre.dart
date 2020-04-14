46fcc1f0d645b30bbad8530f28b549f80f68fafe
Ashu Sharma <ashu7073657271@gmail.com>
2020-04-14 09:21:03 +0530

    Modify layout of testig centers and add support for translation

--- lib/testingCentre.dart
+++ lib/testingCentre.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'getLangCode.dart' as lang;
 
 class TestingCentre extends StatefulWidget {
   @override
@@ -30,6 +33,23 @@ List<String> states = [];
 List<int> centres = [];
 
 class _TestingCentreState extends State<TestingCentre> {
+  List<String> translatedState = [];
+  void translate() async {
+    String langCode = await lang.prefs();
+    for(int i = 0; i < states.length;i++) {
+      String text = states[i];
+      String url = "https://translation.googleapis.com/language/translate/v2?target=$langCode&key=AIzaSyAu7bUrwnWzbfN2lK-zGxdf-KHbzvm-PNA&q=$text";
+      http.Response response = await http.get(url);
+      Map content = json.decode(response.body);
+      if(!translatedState.contains(content["data"]["translations"][0]["translatedText"])) {
+        translatedState.add(content["data"]["translations"][0]["translatedText"]);
+      }
+    }
+    setState(() {
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
@@ -59,50 +79,67 @@ class _TestingCentreState extends State<TestingCentre> {
                   }
                 }
                 states.sort((a, b) => a.toString().compareTo(b.toString()));
-                return ListView.builder(
-                  itemCount: states.length,
+                translate();
+                if(translatedState.isNotEmpty) {
+                  return ListView.builder(
+                  itemCount: translatedState.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    testingCentresDetails(states[index])));
-                      },
-                      child: Container(
-                        height: 60,
-                        margin: EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                              colors: index % 2 == 0 ? color1 : color2,
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight),
-                          borderRadius: BorderRadius.circular(15.0),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black45,
-                                blurRadius: 15.0,
-                                offset: Offset.fromDirection(1.0, 10.0))
-                          ],
+                    return AnimationConfiguration.staggeredList(
+                      duration: Duration(milliseconds: 600),
+                      position: index,
+                      child: SlideAnimation(
+                        horizontalOffset: 50.0,
+                        verticalOffset: 50.0,
+                        child: FadeInAnimation(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          testingCentresDetails(
+                                              states[index])));
+                            },
+                            child: Container(
+                              height: 60,
+                              margin: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                    colors: index % 2 == 0 ? color1 : color2,
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight),
+                                borderRadius: BorderRadius.circular(15.0),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.black45,
+                                      blurRadius: 15.0,
+                                      offset: Offset.fromDirection(1.0, 10.0))
+                                ],
+                              ),
+                              child: Center(
+                                  child: Text(
+                                translatedState[index],
+                                style: TextStyle(
+                                    fontSize: 20.0, color: Colors.white),
+                              )),
+                            ),
+                          ),
                         ),
-                        child: Center(
-                            child: Text(
-                          states[index],
-                          style: TextStyle(fontSize: 20.0, color: Colors.white),
-                        )),
                       ),
                     );
                   },
                 );
+                } else {
+                  return Center(child: SpinKitChasingDots(color: Colors.black,),);
+                }
+                
               } else {
                 return Container(
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.height,
                   child: Center(
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.black,
-                      strokeWidth: 10,
+                    child: SpinKitChasingDots(
+                      color: Colors.black,
                     ),
                   ),
                 );
@@ -114,12 +151,17 @@ class _TestingCentreState extends State<TestingCentre> {
 }
 
 Widget testingCentresDetails(String state) {
+  void translate() async {
+    String langCode = await lang.prefs();
+    String title 
+  }
+
   return Scaffold(
     appBar: AppBar(
-      backgroundColor: Color(0xFF3180e4),
+      backgroundColor: Colors.white,
       title: Text(
         state,
-        style: TextStyle(fontSize: 30.0),
+        style: TextStyle(fontSize: 30.0, color: Colors.black),
       ),
       elevation: 0.0,
       automaticallyImplyLeading: false,
@@ -127,12 +169,7 @@ Widget testingCentresDetails(String state) {
     body: Container(
         width: width,
         height: height,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color(0xFF3180e4),
-            Color(0xFF564dc2),
-          ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
-        ),
+        color: Colors.white,
         child: FutureBuilder(
           future: getUri(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
@@ -143,26 +180,36 @@ Widget testingCentresDetails(String state) {
                 itemBuilder: (BuildContext context, int index) {
                   if (content[index]["state"] == state) {
                     return Container(
-                      margin: EdgeInsets.all(5.0),
+                      margin: EdgeInsets.all(10.0),
                       //height: 100.0,
                       padding: EdgeInsets.all(10.0),
                       decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(15.0),
-                          boxShadow: [
-                            BoxShadow(color: Colors.black26, blurRadius: 10.0)
-                          ]),
+                        gradient: LinearGradient(
+                            colors: index % 2 == 0 ? color1 : color2,
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight),
+                        borderRadius: BorderRadius.circular(15.0),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black45,
+                              blurRadius: 5.0,
+                              offset: Offset.fromDirection(1.0, 3.0))
+                        ],
+                      ),
                       child: Column(
                         children: <Widget>[
                           Text(
                             content[index]["name"],
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                                fontSize: 15.0, fontWeight: FontWeight.bold),
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
                           ),
                           Text(
                             content[index]["detail"],
-                            style: TextStyle(fontSize: 14.0),
+                            style:
+                                TextStyle(fontSize: 14.0, color: Colors.white),
                           ),
                         ],
                       ),
@@ -174,9 +221,8 @@ Widget testingCentresDetails(String state) {
               );
             } else {
               return Center(
-                child: CircularProgressIndicator(
-                  backgroundColor: Colors.black,
-                  strokeWidth: 20.0,
+                child: SpinKitChasingDots(
+                  color: Colors.black,
                 ),
               );
             }
