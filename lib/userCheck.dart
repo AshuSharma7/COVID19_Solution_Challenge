9a104b4b301aab3e97a52e321781af11c908c2f1
Ashu Sharma <ashu7073657271@gmail.com>
2020-04-21 01:51:59 +0530

    Add splash Page

--- lib/userCheck.dart
+++ lib/userCheck.dart
@@ -1,13 +1,18 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_localization_master/pages/newDeclaration.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:async';
 
-import 'bottomNavBar.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_localization_master/pages/LanguagePage.dart';
+import 'package:flutter_localization_master/pages/bottomNavBar.dart';
+import 'package:flutter_localization_master/pages/coronaMonitor.dart';
+import 'package:flutter_localization_master/pages/homePage.dart';
 
-import 'DeclarationForm.dart';
-import 'LanguagePage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localization_master/pages/DeclarationForm.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'newDeclaration.dart';
 
 class CheckUser extends StatefulWidget {
   @override
@@ -19,9 +24,49 @@ Future<SharedPreferences> getPrefs() async {
   return prefs;
 }
 
+final reference = FirebaseDatabase.instance.reference();
+
 class _CheckUserState extends State<CheckUser> {
-  @override
-  void initState() {
+  double lat, long;
+  Position position;
+  StreamSubscription<Position> positionStream;
+  getLocation() async {
+    var geolocator = Geolocator();
+    var locationOptions =
+        LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 1);
+
+    positionStream = geolocator
+        .getPositionStream(locationOptions)
+        .listen((Position position) {
+      if (position != null) {
+        setState(() {
+          lat = position.latitude;
+          long = position.longitude;
+        });
+      } else {
+        setState(() {
+          lat = 0.0;
+          long = 0.0;
+        });
+      }
+    });
+    if (lat != 0.0 || lat != null) {
+      getPrefs().then((value) {
+        reference.child(value.getString('aadhar')).set({
+          'latitude': lat,
+          'longitude': long,
+          "username": value.getString('aadhar')
+        });
+      });
+    }
+  }
+
+  startTime() async {
+    var _duration = new Duration(seconds: 5);
+    return new Timer(_duration, check);
+  }
+
+  void check() {
     getPrefs().then((value) {
       SharedPreferences prefs = value;
       if (prefs.getString('aadhar') != null) {
@@ -29,32 +74,73 @@ class _CheckUserState extends State<CheckUser> {
             prefs.getBool('declared') == null) {
           Navigator.pushAndRemoveUntil(
               context,
-              CupertinoPageRoute(builder: (context) => SelectMembers()),
+              MaterialPageRoute(builder: (context) => CoronaMonitor()),
               (_) => false);
         } else {
           Navigator.pushAndRemoveUntil(
               context,
-              CupertinoPageRoute(builder: (context) => BottomnavBar()),
+              MaterialPageRoute(builder: (context) => BottomnavBar()),
               (_) => false);
         }
       } else {
         Navigator.pushAndRemoveUntil(
             context,
-            CupertinoPageRoute(builder: (context) => LanguagePage()),
+            MaterialPageRoute(builder: (context) => LanguagePage()),
             (_) => false);
       }
     });
+  }
 
+  @override
+  void initState() {
     // TODO: implement initState
     super.initState();
+    startTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(child: SpinKitChasingDots(color: Colors.black)),
-      ),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              image: DecorationImage(
+                  image: AssetImage("assets/images/splash.jpg"))),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 100.0),
+                child: SpinKitFadingCube(
+                  color: Colors.black,
+                  size: 30.0,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image(
+                    image: AssetImage(
+                      "assets/images/indcovid.jpg",
+                    ),
+                    width: 55,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "ANGEL\nGUDIA'S\nCOVID19\nWARRIOR",
+                    style: TextStyle(fontSize: 10.0),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              )
+            ],
+          )),
     );
   }
 }
